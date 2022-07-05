cf0d0b1ebbeb7b55ea33c37e3aa66de2e9edb198
Amy037 <108747109+Amy037@users.noreply.github.com>
2022-07-05 17:43:41 +0200

    Create test.js
    test

---
+++ test.js
@@ -0,0 +1,18 @@
+/*Plan
+Test push, Git
+Nettside?
+
+Finn kode til map
+HTML.index
+
+Hent data fra API
+Funksjoner js, design css
+
+- A clear README in the repository with the following contents:
+  - How should the application be used?
+  - A short description of the technologies used.
+  - What problems does the application solve?
+  - How do you build the application?
+  - How do you run the application?
+  
+*/
